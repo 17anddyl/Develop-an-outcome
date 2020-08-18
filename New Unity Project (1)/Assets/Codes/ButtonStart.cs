c015916b894cf72238d15d93d6ecea353fa2646a
17anddyl <54822982+17anddyl@users.noreply.github.com>
2020-08-19 09:56:13 +1200

    Day 2

---
+++ New Unity Project (1)/Assets/Codes/ButtonStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class ButtonStart : MonoBehaviour
+{
+    public Button yourButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(Update);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("You have clicked the button");    
+    }
+}
