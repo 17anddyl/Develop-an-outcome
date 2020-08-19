cb59ea5b7df39d1e18e432a517a11854230baf48
17anddyl <54822982+17anddyl@users.noreply.github.com>
2020-08-20 10:59:53 +1200

    yes

---
+++ New Unity Project (1)/Assets/Scripts/Titlescreen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Titlescreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void onClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
