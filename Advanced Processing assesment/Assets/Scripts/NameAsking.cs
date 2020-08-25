94b4fcbb7b76e8f096fa51655e28fc66c48930ef
17anddyl <54822982+17anddyl@users.noreply.github.com>
2020-08-25 15:13:06 +1200

    yes

---
+++ Advanced Processing assesment/Assets/Scripts/NameAsking.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameAsking : MonoBehaviour
+{
+    public ImputField nameImport;
+
+
+    string playerName;
+    public string PlayerName;
+    {
+        get{return playerName; }
+        
+    }
+}
