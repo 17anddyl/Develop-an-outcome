3eeee471d938d727a0e41d8a1cc1171f99308549
17anddyl <54822982+17anddyl@users.noreply.github.com>
2020-09-02 09:59:13 +1200

    yes

--- Advanced Processing assesment/Assets/Scripts/QuestionManager.cs
+++ Advanced Processing assesment/Assets/Scripts/QuestionManager.cs
@@ -6,7 +6,29 @@ using TMPro;
 public class QuestionManager : MonoBehaviour
 {
     public TextMeshProUGUI Questions;
+    public int newQuestion = 1;
+    public string questionChange;
+
+
+    public void Update()
+    {
+        Questions.text = questionChange;
+
+        if (newQuestion == 1) questionChange = "Yes";
+
+        if (newQuestion == 2) questionChange = "No";
+    }
 
     public void QuestionAsk()
+    {
+        Questions.text = questionChange;
+        if (newQuestion == 1) questionChange = "Yes"; 
+            
+    }
+
+    public void OnClick()
+    {
+        newQuestion += 1;
+    }
 
 }
